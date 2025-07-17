84834e52d6d065294a7adecd04861955d66c5e5e
OnlyTrable <wrangler2005@gmail.com>
2025-07-17 02:18:23 +0200

    зробив

--- src/components/layout/main/index.jsx
+++ src/components/layout/main/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import FlexContainer from '@/components/ui/flexContainer';
 import styles from './styles.module.css';
+
+// Spotify імпортуємо як URL, бо він для img
 import spotifyLogo from '@/assets/icons/spotify.svg';
-import appleLogo from '@/assets/icons/apple-green.svg';
-import googleLogo from '@/assets/icons/google-green.svg';
-import xLogo from '@/assets/icons/x-green.svg';
+
+// Інші іконки імпортуємо як React-компоненти за допомогою '?react'
+import AppleLogo from '@/assets/icons/apple.svg?react';
+import GoogleLogo from '@/assets/icons/google.svg?react';
+import XLogo from '@/assets/icons/x.svg?react';
 
 
 function Main () {
@@ -16,13 +20,14 @@ function Main () {
                 </FlexContainer>
                 <FlexContainer className={styles.other}>
                     <h1>LIFE IS WASTED ON THE LIVING</h1>
-                    <p>Sign in</p>
+                        <p>Sign in</p>
                     <p>with</p>
                 <FlexContainer className={styles.logosouter}>
                     <FlexContainer className={styles.logos}>
-                        <img className={`${styles.logo} ${styles.socialLogo}`} src={appleLogo} alt="Apple logo" />
-                        <img className={`${styles.logo} ${styles.socialLogo}`} src={googleLogo} alt="Google logo" />
-                        <img className={`${styles.logo} ${styles.socialLogo}`} src={xLogo} alt="X logo" />
+                        {/* Тепер AppleLogo, GoogleLogo, XLogo будуть React-компонентами */}
+                        <AppleLogo className={`${styles.logo} ${styles.socialLogo}`} aria-label="Apple logo" />
+                        <GoogleLogo className={`${styles.logo} ${styles.socialLogo}`} aria-label="Google logo" />
+                        <XLogo className={`${styles.logo} ${styles.socialLogo}`} aria-label="X logo" />
                     </FlexContainer>
                 </FlexContainer>
             </FlexContainer>
